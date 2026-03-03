26662b2fb3af8a0c52571bd3eb3b7cc0bdd713cb
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2026-03-03 17:54:14 +0400

    Add project files.

---
+++ Villa.WebUI/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using Villa.DataAccess.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+var mongoDatabase=new MongoClient(builder.Configuration.GetConnectionString("MongoConnection")).GetDatabase(builder.Configuration.GetSection("DatabaseName").Value);
+
+builder.Services.AddDbContext<VillaContext>(options =>
+{
+    options.UseMongoDB(mongoDatabase.Client,mongoDatabase.DatabaseNamespace.DatabaseName);
+});
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
