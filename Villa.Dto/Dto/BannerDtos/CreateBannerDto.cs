6ddfeb28d8c6752846ed3d74a20b2e2227bc0aa2
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2026-03-06 16:06:07 +0400

    #13 Asp.Net Core 7.0 MongoDb EF Core Villa Projesi - Banner Controller Index is added

---
+++ Villa.Dto/Dto/BannerDtos/CreateBannerDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villa.Dto.Dto.BannerDtos
+{
+    public class CreateBannerDto
+    {
+        public string City { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Villa.WebUI/Mapping/GeneralMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Villa.Dto.Dto.BannerDtos;
+using Villa.Entity.Entities;
+
+namespace Villa.WebUI.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<ResultBannerDto,Banner>().ReverseMap(); 
+            CreateMap<UpdateBannerDto,Banner>().ReverseMap(); 
+            CreateMap<CreateBannerDto,Banner>().ReverseMap(); 
+
+        }
+    }
+}
